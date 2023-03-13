cd579ad007231b78e41fc13a932e1d0ceda59b67
Luca-Sberna <sberna.luca.ibiza@gmail.com>
2023-03-13 17:16:17 +0100

    adj-class

---
+++ g1/compito/pet.js
@@ -0,0 +1,9 @@
+class Pet {
+    constructor(petName, ownerName, specifies, breed) {
+        this.petName = petName;
+        this.ownerName = ownerName;
+        this.specifies = specifies;
+        this.breed = breed;
+    }
+
+}
---
+++ g1/compito/utente.js
@@ -0,0 +1,10 @@
+class User {
+    constructor(firstName, surName, age, location) {
+        this.firstName = firstName;
+        this.surName = surName;
+        this.age = age;
+        this.location = location;
+    }
+
+}
+
