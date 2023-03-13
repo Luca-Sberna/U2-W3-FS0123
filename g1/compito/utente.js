d8d5b7475e12b8fe9df461c85fdfcfaadb329a03
Luca-Sberna <sberna.luca.ibiza@gmail.com>
2023-03-13 17:23:26 +0100

    add-user

--- g1/compito/utente.js
+++ g1/compito/utente.js
@@ -6,5 +6,19 @@ class User {
         this.location = location;
     }
 
+    compareAge(otherUser) {
+        if (this.age > otherUser.age) {
+            return `${this.firstName} è più vecchio di ${otherUser.firstName}`;
+        } else if (this.age < otherUser.age) {
+            return `${this.firstName} è più giovane di ${otherUser.firstName}`;
+        } else {
+            return `${this.firstName} ha la stessa età di ${otherUser.firstName}`;
+        }
+    }
+
 }
 
+const x = new User("Marco", "Rossi", 30, "Roma");
+const y = new User("Giulia", "Bianchi", 25, "Milano");
+console.log(x.compareAge(y));
+console.log(y.compareAge(x)); 
